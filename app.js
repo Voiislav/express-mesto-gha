3a55e03ae84e521a7ff0d3962698de0f36eda459
Voiislav <vojmitrovic@gmail.com>
2023-12-03 11:11:26 +0100

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
+
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
