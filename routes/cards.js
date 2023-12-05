d8178c304f3be7f7ede20c381a09a76025f11a7d
Voiislav <vojmitrovic@gmail.com>
2023-12-05 20:09:13 +0100

    fix: add refs to card model

--- routes/cards.js
+++ routes/cards.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const { getAllCards, createCard, deleteCard, addLike, removeLike } = require('../controllers/cards');
+const {
+  getAllCards, createCard, deleteCard, addLike, removeLike,
+} = require('../controllers/cards');
 
 router.get('/', getAllCards);
 router.post('/', createCard);
