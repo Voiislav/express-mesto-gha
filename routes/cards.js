b7425d5c56572d718e6ef5144f568a6b911e6747
Voiislav <vojmitrovic@gmail.com>
2023-12-05 20:05:05 +0100

    feat: add routers for likes

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const { getAllCards, createCard, deleteCard } = require('../controllers/cards');
+const { getAllCards, createCard, deleteCard, addLike, removeLike } = require('../controllers/cards');
 
-router.get('/cards', getAllCards);
+router.get('/', getAllCards);
 router.post('/', createCard);
 router.delete('/:cardId', deleteCard);
+router.put('/:cardId/likes', addLike);
+router.delete('/:cardId/likes', removeLike);
 
 module.exports = router;
