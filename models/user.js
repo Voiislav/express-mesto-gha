1f572ee76e12c5bd6e18b8f48caff8d2c7194aa6
Voiislav <vojmitrovic@gmail.com>
2023-12-13 19:48:47 +0100

    feat: add validation of avatar value

--- models/user.js
+++ models/user.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    match: /^(https?:\/\/)?(www\.)?[\w\-._~:/?#[\]@!$&'()*+,;=,]+#?$/,
   },
   email: {
     type: String,
