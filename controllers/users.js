5589eda705f6ce5d8fd76460a22cb30ec6b09190
Voiislav <vojmitrovic@gmail.com>
2023-12-13 19:08:00 +0100

    feat: create token and save it to cookie

--- controllers/users.js
+++ controllers/users.js
@@ -2,9 +2,11 @@ const User = require('../models/user');
 
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+
 const { sendErrorResponse } = require('../utils/errorResponse');
 
-const { ERROR_NOT_FOUND, ERROR_BAD_REQUEST, SERVER_ERROR } = require('../utils/errorCodes');
+const { ERROR_NOT_FOUND, ERROR_BAD_REQUEST, SERVER_ERROR, ERROR_UNAUTHORIZED } = require('../utils/errorCodes');
 
 module.exports.createUser = (req, res) => {
   const {
@@ -86,4 +88,26 @@ module.exports.updateAvatar = (req, res) => {
       }
       return sendErrorResponse(res, SERVER_ERROR, error.message);
     });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+  const user = User.findOne({ email })
+  .then((user) => {
+    if (!user) {
+      return Promise.reject({ ERROR_UNAUTHORIZED: true, message: 'Неправильные почта или пароль' });
+    }
+    return bcrypt.compare(password, user.password);
+  })
+  .then((matched) => {
+    if (!matched) {
+      return Promise.reject({ ERROR_UNAUTHORIZED: true, message: 'Неправильные почта или пароль' });
+    }
+    const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+    res.cookie('jwt', token, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 });
+    res.json({ message: 'Авторизация прошла успешно', token });
+  })
+  .catch((error) => {
+    return sendErrorResponse(res, SERVER_ERROR, error.message);
+  });
 };
