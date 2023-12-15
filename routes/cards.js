5fb74031a27dc2d610bf4717225ed5a72e6c22ba
Voiislav <vojmitrovic@gmail.com>
2023-12-15 12:33:09 +0100

    feat: separate validation and routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,41 +1,18 @@
 const express = require('express');
 
-const { celebrate, Joi, Segments } = require('celebrate');
-
 const router = express.Router();
 const {
   getAllCards, createCard, deleteCard, addLike, removeLike,
 } = require('../controllers/cards');
 
-const createCardSchema = {
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().uri().required(),
-  }),
-};
-
-const deleteCardSchema = {
-  [Segments.PARAMS]: Joi.object().keys({
-    cardId: Joi.string().hex().length(24).required(),
-  }),
-};
-
-const addLikeSchema = {
-  [Segments.PARAMS]: Joi.object().keys({
-    cardId: Joi.string().hex().length(24).required(),
-  }),
-};
-
-const removeLikeSchema = {
-  [Segments.PARAMS]: Joi.object().keys({
-    cardId: Joi.string().hex().length(24).required(),
-  }),
-};
+const {
+  createCardSchema, deleteCardSchema, addLikeSchema, removeLikeSchema,
+} = require('../middlewares/validation');
 
 router.get('/', getAllCards);
-router.post('/', celebrate(createCardSchema), createCard);
-router.delete('/:cardId', celebrate(deleteCardSchema), deleteCard);
-router.put('/:cardId/likes', celebrate(addLikeSchema), addLike);
-router.delete('/:cardId/likes', celebrate(removeLikeSchema), removeLike);
+router.post('/', createCardSchema, createCard);
+router.delete('/:cardId', deleteCardSchema, deleteCard);
+router.put('/:cardId/likes', addLikeSchema, addLike);
+router.delete('/:cardId/likes', removeLikeSchema, removeLike);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,21 @@
 const express = require('express');
 
-const { celebrate, Joi, Segments } = require('celebrate');
-
 const router = express.Router();
 const {
   getCurrentUser, getAllUsers, getUserById, updateProfile, updateAvatar,
 } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 
-const updateProfileSchema = {
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-  }),
-};
-
-const updateAvatarSchema = {
-  [Segments.BODY]: Joi.object().keys({
-    avatar: Joi.string().uri(),
-  }),
-};
-
-const getUserByIdSchema = {
-  [Segments.PARAMS]: Joi.object().keys({
-    userId: Joi.string().hex().length(24).required(),
-  }),
-};
+const {
+  updateProfileSchema,
+  updateAvatarSchema,
+  getUserByIdSchema,
+} = require('../middlewares/validation');
 
 router.get('/', getAllUsers);
-router.get('/:userId', celebrate(getUserByIdSchema), getUserById);
-router.patch('/me', celebrate(updateProfileSchema), updateProfile);
-router.patch('/me/avatar', celebrate(updateAvatarSchema), updateAvatar);
+router.get('/:userId', getUserByIdSchema, getUserById);
+router.patch('/me', updateProfileSchema, updateProfile);
+router.patch('/me/avatar', updateAvatarSchema, updateAvatar);
 router.get('/me', auth, getCurrentUser);
 
 module.exports = router;
